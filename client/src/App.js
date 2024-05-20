73aae9ba46def1162fa87aeec03f414d5b6ec043
Dheeraj K P <dheerudheeraj033@gmail.com>
2024-05-20 21:53:41 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>Messenger APP</h2>
+    <form type='submit'>
+      <input placeholder='Enter the message'></input>
+      <button>Enter </button>
+    </form>
+    </div>
+  );
+}
+
+export default App;
